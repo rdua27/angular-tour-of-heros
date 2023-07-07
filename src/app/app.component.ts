cd7196b0eeb0d5860818b7a07d7b387cddf8ef60
rdua27 <rajatdua1994@gmail.com>
2023-07-07 19:45:34 +0530

    make app component standlone and remove app modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heros';
+  title = 'Tour of Heros';
 }
