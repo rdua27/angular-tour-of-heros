b0fb3f8b41a1537f62ae938f7834d9415a5ed605
rdua27 <rajatdua1994@gmail.com>
2023-07-08 01:04:29 +0530

    add hero details component and its service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROS } from './mock-heros';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor() {}
+  getHeros(): Observable<Hero[]> {
+    const heros = of(HEROS);
+    return heros;
+  }
+}
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
-import { HEROS } from '../mock-heros';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css'],
 })
-export class HerosComponent {
-  heros: Hero[] = HEROS;
+export class HerosComponent implements OnInit {
+  heros: Hero[] = [];
   selectedHero?: Hero;
+  heroService: HeroService = inject(HeroService);
+
+  constructor() {}
+  ngOnInit(): void {
+    this.getHeros();
+  }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+  getHeros(): void {
+    this.heroService.getHeros().subscribe((heros) => (this.heros = heros));
   }
 }
